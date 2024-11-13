a68124dd262e493148d12ecb03a9a5832781ce8a
amitVaibhavTiwari <amitvaibhavtiwari@gmail.com>
2024-11-13 21:16:16 +0530

    added nextjs

--- src/Components/Page-changer/PageChanger.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useEffect } from "react";
-export const PageChanger = ({ children }) => {
-  useEffect(() => {
-    window.scrollTo(10, 0);
-  }, []);
-  return <div className="animate-wiggle">{children}</div>;
-};
